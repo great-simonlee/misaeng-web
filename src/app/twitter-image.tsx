616e57f824c083a8d6b4a09622f83c2e738bc3cf
daniel.lee <daniellee@misaenglabs.com>
2026-04-29 10:57:43 +0900

    feat: update twitter-image export with new properties for edge runtime

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,2 +1,10 @@
-export { alt, contentType, runtime, size } from './opengraph-image'
+export const runtime = 'edge'
+
+export const alt = 'Misaeng – Ellieo verified housing platform'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+export const contentType = 'image/png'
+
 export { default } from './opengraph-image'
